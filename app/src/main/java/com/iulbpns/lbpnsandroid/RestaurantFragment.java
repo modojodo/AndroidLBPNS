562c5b1ea787f72f650e6cef74e994ae204ab5e9
Syed Asad Murtaza <asadmurtaza110@gmail.com>
2015-11-17 02:52:46 +0500

    ExpandedListView added.

--- app/src/main/java/com/iulbpns/lbpnsandroid/RestaurantFragment.java
+++ app/src/main/java/com/iulbpns/lbpnsandroid/RestaurantFragment.java
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.zip.Inflater;
 
 /**
@@ -14,11 +18,64 @@ import java.util.zip.Inflater;
  */
 public class RestaurantFragment extends Fragment {
 
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_restaurant,container,false);
+
+
+        expListView = (ExpandableListView) rootView.findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
         return rootView;
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("KFC");
+        listDataHeader.add("Pizza Hut");
+        listDataHeader.add("Burger King");
+
+        // Adding child data
+        List<String> kfc = new ArrayList<String>();
+        kfc.add("Burger");
+        kfc.add("Chicken Wings");
+        kfc.add("Sandwich");
+        kfc.add("Fajita Roll");
+
+
+        List<String> pizzahut = new ArrayList<String>();
+        pizzahut.add("Chicken Tikka");
+        pizzahut.add("Fajita");
+        pizzahut.add("Afghani Tikka");
+        pizzahut.add("Garlic Bread");
+
+
+        List<String> bk = new ArrayList<String>();
+        bk.add("Chicken Burger");
+        bk.add("Jumbo Zinger");
+        bk.add("Club Sandwich");
+        bk.add("Beef Burger");
+
+
+        listDataChild.put(listDataHeader.get(0), kfc); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), pizzahut);
+        listDataChild.put(listDataHeader.get(2), bk);
     }
 }
